63b5f4e9b74cf6aeae2559d3afeb5c57c255d1b1
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-05-27 01:56:04 -0700

    Olykir gave me the idea
    so uhh... this isn't okay... but... it's too funny not to be done... this is crossing a line somewhere.... but I made it funny enough it should be good....

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ global.isSleeping = false; // can use the variable anywhere
 const kate = {
 	sleep: require('./lib/sleep'),
 	wake: require('./lib/wake'),
+	meth: require('./lib/meth')
 };
 
 module.exports = kate;
