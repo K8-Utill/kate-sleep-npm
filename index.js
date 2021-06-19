9877fd9a01d674f6a661bc68d0d117f9c5c54b3d
Hawks <56137888+Official-Hawks@users.noreply.github.com>
2021-06-19 18:15:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ global.isSleeping = false; // can use the variable anywhere
 global.isCozy = false;
 
 const kate = {
-	sleep: require('./lib/sleep'),
-	wake: require('./lib/wake'),
-	meth: require('./lib/meth'),
-	sleepover: require('./lib/sleepover'),
-	say: require('./lib/say'),
-	color: require('./lib/color'),
-	get isSleeping() {
-		return global.isSleeping;
-	},
-	get isCozy() {
-		return global.isCozy;
-	},
+   sleep: require('./lib/sleep'),
+   wake: require('./lib/wake'),
+   meth: require('./lib/meth'),
+   sleepover: require('./lib/sleepover'),
+   say: require('./lib/say'),
+   color: require('./lib/color'),
+   get isSleeping() {
+      return global.isSleeping;
+   },
+   get isCozy() {
+      return global.isCozy;
+   },
 };
 
 module.exports = kate;
