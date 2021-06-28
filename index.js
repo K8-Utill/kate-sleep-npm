4f6376ad54ba9ec60691cc35b952db3ccec821b9
e-boi <hiboysismyoufake6789@gmail.com>
2021-06-28 09:35:54 -0400

    Revert "kate upooded api so I do to"
    This reverts commit a8f7bb2f889918663c809bc34e9f0ce9134e2177.

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 global.isSleeping = false; // can use the variable anywhere
 global.isCozy = false;
 
-const stuffForKate = require('./lib');
-
 const kate = {
-	sleep: stuffForKate.sleep,
-	wake: stuffForKate.wake,
-	meth: stuffForKate.meth,
-	sleepover: stuffForKate.sleepover,
-	say: stuffForKate.say,
-	color: stuffForKate.color,
-	kate: stuffForKate.kate,
-	layout: stuffForKate.layout,
-	getColor: stuffForKate.getColor,
+	sleep: require('./lib/sleep'),
+	wake: require('./lib/wake'),
+	meth: require('./lib/meth'),
+	sleepover: require('./lib/sleepover'),
+	say: require('./lib/say'),
+	color: require('./lib/color'),
 	get isSleeping() {
 		return global.isSleeping;
 	},
